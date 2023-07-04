445f03b2cbc345b814b68508d8e9b7c421ac402a
NWPlayer123 <NWPlayer123@users.noreply.github.com>
2023-07-03 23:06:42 -0600

    Update to latest clap version
    Bumped lto to fat since I have a faster computer and it costs a second, will revert if it becomes a problem

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
+use clap::{arg, crate_authors, crate_description, crate_version, ArgGroup, Command, Id};
+use orthrus_yaz0 as yaz0;
 use std::fs::File;
 use std::io::prelude::*;
-use clap::{AppSettings, Arg, Command, SubCommand};
-use orthrus_yaz0 as yaz0;
 
 fn main() {
-    //TODO: allow modules to generate their own subcommand and just register it here
-    let matches = Command::new("Orthrus")
-    .about("A new way to modify games.")
-    .setting(AppSettings::ArgRequiredElseHelp)
-    .subcommand(SubCommand::with_name("decompress")
-                .arg(Arg::with_name("input")
-                    .required(true))
-                .arg(Arg::with_name("output")
-                    .required(true)))
-    .get_matches();
+    let result = Command::new("Orthrus")
+        .author(crate_authors!("\n"))
+        .version(crate_version!())
+        .about(crate_description!())
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("yaz0")
+                .about("Support for Nintendo's Yaz0 compression")
+                .arg_required_else_help(true)
+                .arg(arg!(-d --decompress "Decompress a Yaz0-compressed file"))
+                .arg(arg!(<input> "Input file to be processed"))
+                .arg(arg!(<output> "Output file to write to"))
+                .group(
+                    ArgGroup::new("methods")
+                        .args(["decompress"])
+                        .requires_all(["input", "output"])
+                        .required(true),
+                ),
+        )
+        .get_matches();
 
-    if let Some(matches) = matches.subcommand_matches("decompress") {
-        //unwrap, it is required and will always be a string
-        let input_fn = matches.get_one::<String>("input").unwrap();
-        let output_fn = matches.get_one::<String>("output").unwrap();
+    match result.subcommand() {
+        Some(("yaz0", sub_matches)) => {
+            match sub_matches.get_one::<Id>("methods").unwrap().as_str() {
+                "decompress" => {
+                    let input_fn = sub_matches.get_one::<String>("input").unwrap();
+                    let output_fn = sub_matches.get_one::<String>("output").unwrap();
 
-        let file = yaz0::load(input_fn).unwrap();
-        let mut output = File::create(output_fn).unwrap();
-        output.write(&file).unwrap();
+                    let file = yaz0::load(input_fn).unwrap();
+                    let mut output = File::create(output_fn).unwrap();
+                    output.write(&file).unwrap();
+                }
+                _ => unreachable!("Oops! Forgot to satisfy all methods"),
+            }
+        }
+        _ => unreachable!("Oops! Forgot to satisfy all subcommands"),
     }
 }
